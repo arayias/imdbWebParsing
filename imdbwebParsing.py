5eec717258503414bedcf4576563f2953536317c
andreasr <49249943+papaya123469@users.noreply.github.com>
2020-07-26 21:11:04 +0300

    Add files via upload

---
+++ imdbwebParsing.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[39]:
+
+
+from requests import get
+from bs4 import BeautifulSoup
+
+# obtain html via requests
+for i in range(15,21):
+    url = f'http://www.imdb.com/search/title?release_date=20{i}&sort=num_votes,desc&page=1'
+    response = get(url)
+
+    #parse html via beautiful soup
+    html_soup = BeautifulSoup(response.text, 'html.parser')
+    type(html_soup)
+
+    movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
+
+    for i in range(len(movie_containers)):
+        name = movie_containers[i].h3.a.text
+        year = movie_containers[i].find('span', class_ = 'lister-item-year text-muted unbold').text
+        imdbRating = movie_containers[i].find('div', class_ = 'inline-block ratings-imdb-rating').strong.text
+        genre = (movie_containers[i].find('span', class_ = 'genre').text).strip()
+
+
+
+        print(f'{name} , {year} , {imdbRating} , ({genre})')
+    
+    
+
+    
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
