1727cdd1d8db6d38240fc38408925c4887355764
andreasr <49249943+papaya123469@users.noreply.github.com>
2020-07-26 21:14:42 +0300

    Update imdbwebParsing.py

--- imdbwebParsing.py
+++ imdbwebParsing.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[39]:
 
 
 from requests import get
 from bs4 import BeautifulSoup
 
 # obtain html via requests
-for i in range(15,21):
-    url = f'http://www.imdb.com/search/title?release_date=20{i}&sort=num_votes,desc&page=1'
+for i in range(15,21): # year 2015-2020
+    url = f'http://www.imdb.com/search/title?release_date=20{i}&sort=num_votes,desc&page=1' #only displaying page one to show top 50
     response = get(url)
 
     #parse html via beautiful soup
@@ -19,40 +18,12 @@ for i in range(15,21):
     movie_containers = html_soup.find_all('div', class_ = 'lister-item mode-advanced')
 
     for i in range(len(movie_containers)):
-        name = movie_containers[i].h3.a.text
-        year = movie_containers[i].find('span', class_ = 'lister-item-year text-muted unbold').text
-        imdbRating = movie_containers[i].find('div', class_ = 'inline-block ratings-imdb-rating').strong.text
-        genre = (movie_containers[i].find('span', class_ = 'genre').text).strip()
+        name = movie_containers[i].h3.a.text #locates title
+        year = movie_containers[i].find('span', class_ = 'lister-item-year text-muted unbold').text #locates year
+        imdbRating = movie_containers[i].find('div', class_ = 'inline-block ratings-imdb-rating').strong.text #locates rating
+        genre = (movie_containers[i].find('span', class_ = 'genre').text).strip() #locates genre
 
 
 
         print(f'{name} , {year} , {imdbRating} , ({genre})')
     
-    
-
-    
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
